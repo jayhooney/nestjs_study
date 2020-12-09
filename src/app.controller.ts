df06c3ddf64c604c63800f718b2025fc097b272d
jay <dev_jhlee@wisenut.co.kr>
2020-12-09 17:40:28 +0900

    test: test logger
    NestJS 에서 지원하는 logger를 테스트해봄.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  private readonly logger = new Logger(AppController.name);
+
   @Get()
   async helloNestJS(): Promise<string> {
+    this.logger.log(`NORMAL LOG`);
+    this.logger.debug(`DEBUG LOG`);
+    this.logger.warn(`WARN LOG`);
+    this.logger.error(`ERROR LOG`);
+    this.logger.verbose(`VERBOSE LOG`);
+
     return this.appService.helloNestJS();
   }
 }
